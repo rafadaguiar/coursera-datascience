a6a7304176f2a063c624f064edff45f9c83f924e
rafadaguiar <rfna@cin.ufpe.br>
2013-05-19 10:04:47 -0300

    Ex2: Part 4 Done

--- assignment2/queries.sql
+++ assignment2/queries.sql
@@ -6,3 +6,4 @@ select count(term) from Frequency where docid = "10398_txt_earn" and count = 1;
 select count(term) from (select term from Frequency where docid = "10398_txt_earn" and count = 1 
                          UNION select term from Frequency where docid = "925_txt_trade" and count = 1);
 #4
+select count(term) from Frequency where term like 'parliament';
