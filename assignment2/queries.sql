f1485641a32c90b09155cb9d03cfd4028844172a
rafadaguiar <rfna@cin.ufpe.br>
2013-05-19 10:01:14 -0300

    Ex2: Part 3 Done

---
+++ assignment2/queries.sql
@@ -0,0 +1,8 @@
+#1
+select count(*) from Frequency where docid = "10398_txt_earn";
+#2
+select count(term) from Frequency where docid = "10398_txt_earn" and count = 1;
+#3
+select count(term) from (select term from Frequency where docid = "10398_txt_earn" and count = 1 
+                         UNION select term from Frequency where docid = "925_txt_trade" and count = 1);
+#4
