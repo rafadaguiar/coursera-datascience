97f28270bd3db9947842d08cbf47c7922c12fe66
rafadaguiar <rfna@cin.ufpe.br>
2013-05-20 13:56:23 -0300

    Ex2: Part 7,8,9 Done

--- assignment2/queries.sql
+++ assignment2/queries.sql
@@ -11,3 +11,19 @@ select count(term) from Frequency where term like 'parliament';
 select count(docid) from (select sum(count) as c1, docid from Frequency group by docid) where c1 > 300;
 #6
 select distinct count(docid) from (select docid from Frequency Where term = 'transaction' INTERSECT select docid from Frequency Where term = 'world');
+
+#7 (Matrix multiplication with SQL) A[i,k] * B[k,j] = C[i,j]
+select A.row_num,B.col_num,sum(A.value*B.value) from A,B WHERE A.col_num = B.row_num group by A.row_num, B.col_num;
+#8 S = D*D'
+select sum(A.count*B.count) from Frequency A, Frequency B WHERE A.term = B.term and A.docid = '10080_txt_crude' and B.docid = '17035_txt_earn';
+#9
+create view keyword as 
+                        SELECT * FROM frequency
+                        UNION
+                        SELECT 'q' as docid, 'washington' as term, 1 as count 
+                        UNION
+                        SELECT 'q' as docid, 'taxes' as term, 1 as count
+                        UNION 
+                        SELECT 'q' as docid, 'treasury' as term, 1 as count
+                        
+select A.docid,sum(A.count*B.count)as score from keyword A, keyword B WHERE A.term = B.term and B.docid="q" Group By A.docid order by -score limit 10;
